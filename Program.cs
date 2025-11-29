0d3f1afb49fc1924e37641c423f358094e3e303a
javierdebug <jvargas071@gmail.com>
2025-11-29 17:58:51 -0500

    Update summaries logic so is consistent

--- Program.cs
+++ Program.cs
@@ -13,21 +13,20 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+    {
+        int randomValue = Random.Shared.Next(-22, 55);
+        var weatherData = new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                randomValue,
+            new WeatherTextValue(randomValue).checkWeather()
+        );
+
+        return weatherData;
+    }).ToArray();
+
     return forecast;
 }).WithName("GetWeatherForecast");
 
@@ -38,4 +37,54 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class WeatherTextValue(int Value)
+{
+    private string[] summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+    public string checkWeather()
+    {
+        if (Value < 0)
+        {
+            return summaries[0];
+        }
+        else if (Value < 5)
+        {
+            return summaries[1];
+        }
+        else if (Value < 10)
+        {
+            return summaries[2];
+        }
+        else if (Value < 15)
+        {
+            return summaries[3];
+        }
+        else if (Value < 20)
+        {
+            return summaries[4];
+        }
+        else if (Value < 25)
+        {
+            return summaries[5];
+        }
+        else if (Value < 30)
+        {
+            return summaries[6];
+        }
+        else if (Value < 35)
+        {
+            return summaries[7];
+        }
+        else if (Value < 40)
+        {
+            return summaries[8];
+        }
+        else
+        {
+            return summaries[9];
+        }
+    }
+
 }
